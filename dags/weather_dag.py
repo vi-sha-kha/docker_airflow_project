b3ea5c9dd0c085b63e092e62dacd63ee9d5291a9
vi-sha-kha <vishakha.ch.5281@gmail.com>
2024-01-10 20:20:04 +0545

    added task group

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from datetime import timedelta, datetime
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 default_args = {
     'owner': 'airflow',
@@ -21,3 +23,33 @@ with DAG('weather_dag_2',
         start_pipeline = DummyOperator(
             task_id = 'tsk_start_pipeline'
         )
+
+        with TaskGroup(group_id = 'group_a', tooltip= "Extract_from_S3_and_weatherapi") as group_A:
+            create_table_1 = PostgresOperator(
+                task_id='tsk_create_table_1',
+                postgres_conn_id = "postgres_conn",
+                sql= '''  
+                    CREATE TABLE IF NOT EXISTS city_look_up (
+                    city TEXT NOT NULL,
+                    state TEXT NOT NULL,
+                    census_2020 numeric NOT NULL,
+                    land_Area_sq_mile_2020 numeric NOT NULL                    
+                );
+                '''
+            )
+
+            truncate_table = PostgresOperator(
+                task_id='tsk_truncate_table',
+                postgres_conn_id = "postgres_conn",
+                sql= ''' TRUNCATE TABLE city_look_up;
+                    '''
+            )
+
+            uploadS3_to_postgres  = PostgresOperator(
+                task_id = "tsk_uploadS3_to_postgres",
+                postgres_conn_id = "postgres_conn",
+                sql = "SELECT aws_s3.table_import_from_s3('city_look_up', '', '(format csv, DELIMITER '','', HEADER true)', 's3-bucket-weather-api', 'us_city.csv', 'us-east-1');"
+            )
+
+            create_table_1>>truncate_table>>uploadS3_to_postgres
+        start_pipeline>>group_A
